ae0881dbd2802ab5e2802caae41d2e7585155a87
TiagoPachecoRibeiro <tiago.p.ribeiro04@gmail.com>
2024-06-10 23:18:11 +0100

    correcoes

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,25 @@
 // src/i18n.js
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
-import messages from './locales'; // Adjust the path to your locales
 
 Vue.use(VueI18n);
 
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  fr: {
+    message: {
+      hello: 'bonjour le monde'
+    }
+  }
+};
+
 const i18n = new VueI18n({
-  locale: 'en', // Set default locale
-  messages, // Set locale messages
+  locale: 'en', // set locale
+  messages, // set locale messages
 });
 
 export default i18n;
--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ import RouterPrefetch from "vue-router-prefetch";
 import Meta from "vue-meta";
 import App from "./App.vue";
 import router from "./router";
-
-import BlackDashboard from "./plugins/blackDashboard";
 import i18n from "./i18n";
+import BlackDashboard from "./plugins/blackDashboard";
 import "./registerServiceWorker";
 import "@/style.css";
 
