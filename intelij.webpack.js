8a997737c9a24917d28ec4c0955a9ae76dbf2bdd
TiagoPachecoRibeiro <tiago.p.ribeiro04@gmail.com>
2024-06-10 21:06:20 +0100

    html

--- intelij.webpack.js
+++ intelij.webpack.js
@@ -1,12 +1,54 @@
-// This configuration file is not used anywhere in the code, it's a hack to handle InteliJ relative path imports
-// Keep in sync with actual webpack aliases
-
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      vue$: 'vue/dist/vue.esm.js',
     },
+    extensions: ['*', '.js', '.vue', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './static/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'static'),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+    open: true,
   },
 };
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import RouterPrefetch from "vue-router-prefetch";
 import Meta from "vue-meta";
-import App from "./App";
+import App from "./App.vue";
 import router from "./router";
 
 import BlackDashboard from "./plugins/blackDashboard";
